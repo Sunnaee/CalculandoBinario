7f9c0a09b95bc41a8b8927c3631a0dc559f60e2a
martingci <m.carrasco29@ufromail.cl>
2024-08-19 16:18:25 -0400

    Elaboración de algoritmo

--- src/main/java/Binario.java
+++ src/main/java/Binario.java
@@ -1,5 +1,16 @@
 public class Binario {
     public static void main(String[] args) {
-        System.out.println("Binario Test");
+        int number = 0;
+        int [] binario = new int[8];
+        for (int i = 7; i >= 0; i--){
+            int couciente = number/2;
+            int modulo = number%2;
+            binario[i] = modulo;
+            number = couciente;
+        }
+        for (int item : binario) {
+            System.out.print(item);
+        }
+        System.out.println("");
     }
 }
